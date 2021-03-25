f00b3074aa761cbb7b119017d019b2819cb7dec6
Javier Borja <javier.borja@thoughtworks.com>
2021-03-24 22:36:54 -0600

    Finishes burger controls and order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,6 +2,7 @@ import React from "react";
 // import classes from "./OrderSummary.module.css";
 
 import Aux from "../../../hoc/Aux";
+import Button from "../../UI/Button/Button";
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
@@ -19,7 +20,14 @@ const orderSummary = (props) => {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p><strong>Total Price: ${props.totalPrice.toFixed(2)}</strong></p>
       <p>Continue to checkout?</p>
+      <Button buttonType="Danger" clicked={props.purchaseCancelled}>
+        Cancel
+      </Button>
+      <Button buttonType="Success" clicked={props.purchaseContinue}>
+        Continue
+      </Button>
     </Aux>
   );
 };
