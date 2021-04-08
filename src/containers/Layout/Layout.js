9591ffae5c5df0fdf254910a8c4d00b68d56947f
Javier Borja <javier.borja@thoughtworks.com>
2021-04-07 23:14:31 -0500

    changes folder for Layout

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import Aux from "../../hoc/Aux";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
-import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
 import classes from "./Layout.module.css";
 
